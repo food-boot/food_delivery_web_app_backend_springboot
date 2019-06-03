3ac58f8847c522ddf9f5ed4b1a60968423dbb6a8
shehand <sonodewcomplex@gmail.com>
2019-06-03 19:15:01 +0530

    Initial commit

---
+++ src/main/java/com/food/delivery/app/ws/FoodDeliveryWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.food.delivery.app.ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodDeliveryWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodDeliveryWebAppApplication.class, args);
+	}
+
+}
