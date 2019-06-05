37ab12aec148133aa7ab44540aaccc105378d4cb
shehand <sonodewcomplex@gmail.com>
2019-06-05 22:49:25 +0530

    Add use data transfer object class

---
+++ src/main/java/com/food/delivery/app/ws/shared/dto/UserDto.java
@@ -0,0 +1,72 @@
+package com.food.delivery.app.ws.shared.dto;
+
+import java.io.Serializable;
+
+public class UserDto implements Serializable{
+
+	private static final long serialVersionUID = -5382713250745188694L;
+	
+	private long id;
+	private String userId;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String password;
+	private String encryptedPassword;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEncryptedPassword() {
+		return encryptedPassword;
+	}
+
+	public void setEncryptedPassword(String encryptedPassword) {
+		this.encryptedPassword = encryptedPassword;
+	}
+}
