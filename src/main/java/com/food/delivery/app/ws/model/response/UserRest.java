796ed3bd77823156c9da888fcb8195b74c32fd2b
shehand <sonodewcomplex@gmail.com>
2019-06-05 22:48:18 +0530

    Add user response and user request classes

---
+++ src/main/java/com/food/delivery/app/ws/model/response/UserRest.java
@@ -0,0 +1,51 @@
+package com.food.delivery.app.ws.model.response;
+
+public class UserRest {
+	
+	private String userId;
+	private String email;
+	private String firstName;
+	private String lastName;
+	private String picURL;
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getPicURL() {
+		return picURL;
+	}
+
+	public void setPicURL(String picURL) {
+		this.picURL = picURL;
+	}
+}
+	
