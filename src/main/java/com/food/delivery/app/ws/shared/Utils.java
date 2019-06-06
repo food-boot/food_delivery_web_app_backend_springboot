ddc52b73ff6ad0e6f306c401f3ce8eae6d7d4ff5
shehand <sonodewcomplex@gmail.com>
2019-06-06 19:06:00 +0530

    Add utils package

---
+++ src/main/java/com/food/delivery/app/ws/shared/Utils.java
@@ -0,0 +1,29 @@
+package com.food.delivery.app.ws.shared;
+
+import java.security.SecureRandom;
+import java.util.Random;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Utils {
+	
+	private final Random RANDOM = new SecureRandom();
+	private final String ALPHABET = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	private final int ITERATIONS = 10000;
+	private final int KEY_LENGTH = 256;
+	
+	public String generateUserId(int length) {
+		return generateRandomString(length);
+	}
+
+	private String generateRandomString(int length) {
+		StringBuilder returnValue = new StringBuilder(length);
+		
+		for(int i = 0; i < length; i++) {
+			returnValue.append(ALPHABET.charAt(RANDOM.nextInt(ALPHABET.length())));
+		}
+		
+		return new String(returnValue);
+	}
+}
