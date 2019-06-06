b646d3d952a505bbac977344aec2644249c6dd7f
shehand <sonodewcomplex@gmail.com>
2019-06-06 21:31:41 +0530

    Add Bcryptpassword package to the application

--- src/main/java/com/food/delivery/app/ws/FoodDeliveryWebAppApplication.java
+++ src/main/java/com/food/delivery/app/ws/FoodDeliveryWebAppApplication.java
@@ -2,12 +2,19 @@ package com.food.delivery.app.ws;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class FoodDeliveryWebAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FoodDeliveryWebAppApplication.class, args);
+	}
+	
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 
 }
