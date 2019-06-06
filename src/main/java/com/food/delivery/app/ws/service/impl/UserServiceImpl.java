281b7b6401d241b67c0503568e7b23c064519a35
shehand <sonodewcomplex@gmail.com>
2019-06-06 19:05:41 +0530

    Update user public id generation

--- src/main/java/com/food/delivery/app/ws/service/impl/UserServiceImpl.java
+++ src/main/java/com/food/delivery/app/ws/service/impl/UserServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import com.food.delivery.app.ws.io.entity.UserEntity;
 import com.food.delivery.app.ws.io.repository.UserRepository;
 import com.food.delivery.app.ws.service.UserService;
+import com.food.delivery.app.ws.shared.Utils;
 import com.food.delivery.app.ws.shared.dto.UserDto;
 
 @Service
@@ -14,6 +15,9 @@ public class UserServiceImpl implements UserService{
 
 	@Autowired
 	UserRepository userRepository;
+	
+	@Autowired
+	Utils utils;
 	
 	@Override
 	public UserDto createUser(UserDto user) {
@@ -25,7 +29,9 @@ public class UserServiceImpl implements UserService{
 		
 		userEntity.setPicURL("temporory_pic_url");
 		userEntity.setEncryptedPassword("sample_Password");
-		userEntity.setUserId("sample_user_id");
+		
+		String publicUserId = utils.generateUserId(30);
+		userEntity.setUserId(publicUserId);
 		UserEntity storedUserDetails = userRepository.save(userEntity);
 		
 		UserDto returnValue = new UserDto();
