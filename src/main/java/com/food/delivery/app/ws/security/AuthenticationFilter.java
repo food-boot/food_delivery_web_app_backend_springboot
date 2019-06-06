55ce12b038486702d470de59a49a2a038e650f37
shehand <sonodewcomplex@gmail.com>
2019-06-06 22:48:50 +0530

    Update authentication filter with jwt token builder

--- src/main/java/com/food/delivery/app/ws/security/AuthenticationFilter.java
+++ src/main/java/com/food/delivery/app/ws/security/AuthenticationFilter.java
@@ -1,8 +1,11 @@
 package com.food.delivery.app.ws.security;
 
 import java.io.IOException;
+import java.rmi.ServerException;
 import java.util.ArrayList;
+import java.util.Date;
 
+import javax.servlet.FilterChain;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -10,10 +13,14 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.food.delivery.app.ws.model.request.UserLoginRequseModel;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 
 public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
@@ -24,18 +31,32 @@ public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 	}
 
 	@Override
-	public Authentication attemptAuthentication(HttpServletRequest req, HttpServletResponse res) throws AuthenticationException{
+	public Authentication attemptAuthentication(HttpServletRequest req, HttpServletResponse res)
+			throws AuthenticationException {
 
 		try {
 			UserLoginRequseModel creds = new ObjectMapper().readValue(req.getInputStream(), UserLoginRequseModel.class);
 
 			return authenticationManager.authenticate(
 					new UsernamePasswordAuthenticationToken(creds.getEmail(), creds.getPassword(), new ArrayList<>()));
-			
+
 		} catch (IOException e) {
 			throw new RuntimeException(e);
 		}
 	}
-	
-	protected void successfulAuthentication() {}
+
+	@Override
+	protected void successfulAuthentication(HttpServletRequest req, HttpServletResponse res, FilterChain chain,
+			Authentication auth) throws IOException, ServerException {
+
+		String userName = ((User) auth.getPrincipal()).getUsername();
+
+		String token = Jwts.builder()
+				.setSubject(userName)
+				.setExpiration(new Date(System.currentTimeMillis() + SecurityConstants.EXPIRATION_TIME))
+				.signWith(SignatureAlgorithm.HS512, SecurityConstants.TOKEN_SECRET)
+				.compact();
+		
+		res.addHeader(SecurityConstants.HEADER_STRING, SecurityConstants.TOKEN_PREFIX + token);
+	}
 }
