2045330e666e667a8de8f3ef12fb19da72b5e045
shehand <sonodewcomplex@gmail.com>
2019-06-06 21:32:30 +0530

    Update user service class

--- src/main/java/com/food/delivery/app/ws/service/UserService.java
+++ src/main/java/com/food/delivery/app/ws/service/UserService.java
@@ -1,8 +1,10 @@
 package com.food.delivery.app.ws.service;
 
+import org.springframework.security.core.userdetails.UserDetailsService;
+
 import com.food.delivery.app.ws.shared.dto.UserDto;
 
-public interface UserService {
+public interface UserService extends UserDetailsService{
 
 	UserDto createUser(UserDto user);
 }
