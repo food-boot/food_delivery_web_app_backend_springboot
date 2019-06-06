795af069aebe85b05b88e9954766b3bbd8dd7890
shehand <sonodewcomplex@gmail.com>
2019-06-06 22:49:16 +0530

    Update websecurity with filter

--- src/main/java/com/food/delivery/app/ws/security/WebSecurity.java
+++ src/main/java/com/food/delivery/app/ws/security/WebSecurity.java
@@ -23,8 +23,13 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 
-		http.csrf().disable().authorizeRequests().antMatchers(HttpMethod.POST, SecurityConstants.SIGN_UP_URL).permitAll().anyRequest()
-				.authenticated();
+		http.csrf()
+		.disable()
+		.authorizeRequests()
+		.antMatchers(HttpMethod.POST, SecurityConstants.SIGN_UP_URL)
+		.permitAll()
+		.anyRequest()
+		.authenticated().and().addFilter(new AuthenticationFilter(authenticationManager()));
 	}
 	
 	public void configure(AuthenticationManagerBuilder auth) throws Exception {
