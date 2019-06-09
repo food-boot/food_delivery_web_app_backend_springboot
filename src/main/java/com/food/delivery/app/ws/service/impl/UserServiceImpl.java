987d2cc56f814dc9d08398be161a33415b1a80f5
shehand <sonodewcomplex@gmail.com>
2019-06-09 13:04:41 +0530

    Implement the get user by email address method

--- src/main/java/com/food/delivery/app/ws/service/impl/UserServiceImpl.java
+++ src/main/java/com/food/delivery/app/ws/service/impl/UserServiceImpl.java
@@ -58,5 +58,17 @@ public class UserServiceImpl implements UserService{
 		return new User(userEntity.getEmail(), userEntity.getEncryptedPassword(), new ArrayList<>());
 	}
 
+	@Override
+	public UserDto getUser(String email)throws UsernameNotFoundException {
+		
+		UserEntity userEntity = userRepository.findByEmail(email);
+		
+		if(userEntity == null) throw new UsernameNotFoundException(email);
+		
+		UserDto returnValue = new UserDto();
+		BeanUtils.copyProperties(userEntity, returnValue);
+		return returnValue;
+	}
+
 }
 	
