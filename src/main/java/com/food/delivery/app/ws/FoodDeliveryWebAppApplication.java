5e51ed11fcbe829ec4acfd0997ff69f039fd669f
shehand <sonodewcomplex@gmail.com>
2019-06-17 17:41:33 +0530

    Remove cors header in main file

--- src/main/java/com/food/delivery/app/ws/FoodDeliveryWebAppApplication.java
+++ src/main/java/com/food/delivery/app/ws/FoodDeliveryWebAppApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class FoodDeliveryWebAppApplication {
@@ -21,4 +23,5 @@ public class FoodDeliveryWebAppApplication {
 	public SpringApplicationContext springApplicationContext() {
 		return new SpringApplicationContext();
 	}
+
 }
