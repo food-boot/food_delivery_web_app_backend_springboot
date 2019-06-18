9f00c70fc8bad002ebe68f8af425f1b27f6f6e23
shehand <sonodewcomplex@gmail.com>
2019-06-18 20:07:11 +0530

    Add food details response model

---
+++ src/main/java/com/food/delivery/app/ws/model/response/FoodDetailsResponse.java
@@ -0,0 +1,33 @@
+package com.food.delivery.app.ws.model.response;
+
+public class FoodDetailsResponse {
+
+	private String foodId;
+	private String foodName;
+	private float foodPrice;
+
+	public String getFoodId() {
+		return foodId;
+	}
+
+	public void setFoodId(String foodId) {
+		this.foodId = foodId;
+	}
+
+	public String getFoodName() {
+		return foodName;
+	}
+
+	public void setFoodName(String foodName) {
+		this.foodName = foodName;
+	}
+
+	public float getFoodPrice() {
+		return foodPrice;
+	}
+
+	public void setFoodPrice(float foodPrice) {
+		this.foodPrice = foodPrice;
+	}
+
+}
