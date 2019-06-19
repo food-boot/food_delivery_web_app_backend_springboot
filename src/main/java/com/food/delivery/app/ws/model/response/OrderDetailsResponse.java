947205426ee2028e8ab61d8cddcf201218daafe1
shehand <sonodewcomplex@gmail.com>
2019-06-19 21:46:24 +0530

    Add order details response model

---
+++ src/main/java/com/food/delivery/app/ws/model/response/OrderDetailsResponse.java
@@ -0,0 +1,33 @@
+package com.food.delivery.app.ws.model.response;
+
+public class OrderDetailsResponse {
+
+	private String orderId;
+	private float cost;
+	private String items[];
+
+	public String getOrderId() {
+		return orderId;
+	}
+
+	public void setOrderId(String orderId) {
+		this.orderId = orderId;
+	}
+
+	public float getCost() {
+		return cost;
+	}
+
+	public void setCost(float cost) {
+		this.cost = cost;
+	}
+
+	public String[] getItems() {
+		return items;
+	}
+
+	public void setItems(String[] items) {
+		this.items = items;
+	}
+
+}
