48508103b65b857b34641ebdf75e3a0d9a88a6d3
shehand <sonodewcomplex@gmail.com>
2019-06-19 21:45:28 +0530

    Add get user api end point

--- src/main/java/com/food/delivery/app/ws/ui/controller/UsesrController.java
+++ src/main/java/com/food/delivery/app/ws/ui/controller/UsesrController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,32 +23,39 @@ public class UsesrController {
 
 	@Autowired
 	UserService userService;
-	
-	@GetMapping
-	public String getUser() {
-		return "get user method is called";
-	}
-	
+
 	@CrossOrigin
-	@PostMapping
-	public UserRest createUser(@RequestBody UserDetailsRequestModel userDetails) {
+	@GetMapping(path = "/{id}")
+	public UserRest getUser(@PathVariable String id) {
+
+		UserRest returnValue = new UserRest();
 		
-		UserRest returnValue  = new UserRest();
-		
-		UserDto userDto = new UserDto();
-		BeanUtils.copyProperties(userDetails, userDto);
-		
-		UserDto createUser = userService.createUser(userDto);
-		BeanUtils.copyProperties(createUser, returnValue);
+		UserDto userDto = userService.getUserByUserId(id);
+		BeanUtils.copyProperties(userDto, returnValue);
 		
 		return returnValue;
 	}
-	
+
+	@CrossOrigin
+	@PostMapping
+	public UserRest createUser(@RequestBody UserDetailsRequestModel userDetails) {
+
+		UserRest returnValue = new UserRest();
+
+		UserDto userDto = new UserDto();
+		BeanUtils.copyProperties(userDetails, userDto);
+
+		UserDto createUser = userService.createUser(userDto);
+		BeanUtils.copyProperties(createUser, returnValue);
+
+		return returnValue;
+	}
+
 	@PutMapping
 	public String updateUser() {
 		return "update user method is called";
 	}
-	
+
 	@DeleteMapping
 	public String deleteUser() {
 		return "delete user method is called";
