697487f638996af8521966773e1fd1b14cfdab07
shehand <sonodewcomplex@gmail.com>
2019-06-22 20:00:30 +0530

    Implement the user update method

--- src/main/java/com/food/delivery/app/ws/service/impl/UserServiceImpl.java
+++ src/main/java/com/food/delivery/app/ws/service/impl/UserServiceImpl.java
@@ -72,12 +72,30 @@ public class UserServiceImpl implements UserService{
 
 	@Override
 	public UserDto getUserByUserId(String userId) throws UsernameNotFoundException{
-		UserDto returnValue =new UserDto();
+		UserDto returnValue = new UserDto();
 		UserEntity userEntity = userRepository.findByUserId(userId);
 		
 		if(userEntity == null) throw new UsernameNotFoundException(userId);
 		
 		BeanUtils.copyProperties(userEntity, returnValue);
+		return returnValue;
+	}
+
+	@Override
+	public UserDto updateUser(String userId, UserDto user) throws UsernameNotFoundException{
+		
+		UserDto returnValue = new UserDto();
+		
+		UserEntity userEntity = userRepository.findByUserId(userId);
+		
+		if(userEntity == null) throw new UsernameNotFoundException(userId);
+		
+		userEntity.setFirstName(user.getFirstName());
+		userEntity.setLastName(user.getLastName());
+		
+		UserEntity updatedUserDetails = userRepository.save(userEntity);
+		BeanUtils.copyProperties(updatedUserDetails, returnValue);
+		
 		return returnValue;
 	}
 
