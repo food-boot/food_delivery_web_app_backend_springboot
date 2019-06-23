9050bee44e68df6378ea31959bfc0b574f900128
shehand <sonodewcomplex@gmail.com>
2019-06-23 18:21:40 +0530

    Add generate food id method

--- src/main/java/com/food/delivery/app/ws/shared/Utils.java
+++ src/main/java/com/food/delivery/app/ws/shared/Utils.java
@@ -17,6 +17,10 @@ public class Utils {
 		return generateRandomString(length);
 	}
 
+	public String generateFoodId(int length) {
+		return generateRandomString(length);
+	}
+
 	private String generateRandomString(int length) {
 		StringBuilder returnValue = new StringBuilder(length);
 		
