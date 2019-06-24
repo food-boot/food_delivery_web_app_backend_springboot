1908b539b50c3468d73e85ac260cccd5defc32f3
shehand <sonodewcomplex@gmail.com>
2019-06-24 11:48:04 +0530

    Implement all api end points methods

--- src/main/java/com/food/delivery/app/ws/service/impl/FoodServiceImpl.java
+++ src/main/java/com/food/delivery/app/ws/service/impl/FoodServiceImpl.java
@@ -2,6 +2,8 @@ package com.food.delivery.app.ws.service.impl;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import com.food.delivery.app.ws.io.entity.FoodEntity;
 import com.food.delivery.app.ws.io.repository.FoodRepository;
@@ -9,6 +11,7 @@ import com.food.delivery.app.ws.service.FoodService;
 import com.food.delivery.app.ws.shared.Utils;
 import com.food.delivery.app.ws.shared.dto.FoodDto;
 
+@Service
 public class FoodServiceImpl implements FoodService {
 
 	@Autowired
@@ -32,6 +35,50 @@ public class FoodServiceImpl implements FoodService {
 		FoodDto foodDto = new FoodDto();
 		foodDto = modelMapper.map(storedFood, FoodDto.class);
 		return foodDto;
+	}
+
+	@Override
+	public FoodDto getFoodById(String foodId) throws UsernameNotFoundException{
+		
+		FoodDto returnValue = new FoodDto();
+		ModelMapper modelMapper = new ModelMapper();
+		
+		FoodEntity foodEntity = foodRepository.findByFoodId(foodId);
+
+		if(foodEntity == null) throw new UsernameNotFoundException(foodId);
+		
+		returnValue = modelMapper.map(foodEntity, FoodDto.class);
+		return returnValue;
+	}
+
+	@Override
+	public FoodDto updateFoodDetails(String foodId, FoodDto foodDetails) throws UsernameNotFoundException {
+		
+		FoodDto returnValue = new FoodDto();
+		ModelMapper modelMapper = new ModelMapper();
+		
+		FoodEntity foodEntity = foodRepository.findByFoodId(foodId);
+		
+		if(foodEntity == null) throw new UsernameNotFoundException(foodId);
+		
+		foodEntity.setFoodCategory(foodDetails.getFoodCategory());
+		foodEntity.setFoodName(foodDetails.getFoodName());
+		foodEntity.setFoodPrice(foodDetails.getFoodPrice());
+		
+		FoodEntity updatedFoodEntity = foodRepository.save(foodEntity);
+		returnValue = modelMapper.map(updatedFoodEntity, FoodDto.class);
+		
+		return returnValue;
+	}
+
+	@Override
+	public void deleteFoodItem(String id) throws UsernameNotFoundException {
+		
+		FoodEntity foodEntity = foodRepository.findByFoodId(id);
+		if(foodEntity == null) throw new UsernameNotFoundException(id);
+		
+		foodRepository.delete(foodEntity);
+		
 	}
 
 }
