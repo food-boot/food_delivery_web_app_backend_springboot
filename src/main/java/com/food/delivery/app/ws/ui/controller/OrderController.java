8a1224f96c7c55b02dab532309e7e75f91b08863
shehand <sonodewcomplex@gmail.com>
2019-06-25 21:15:26 +0530

    Add create order api end point

--- src/main/java/com/food/delivery/app/ws/ui/controller/OrderController.java
+++ src/main/java/com/food/delivery/app/ws/ui/controller/OrderController.java
@@ -1,15 +1,27 @@
 package com.food.delivery.app.ws.ui.controller;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.food.delivery.app.ws.model.request.OrderDetailsRequestModel;
+import com.food.delivery.app.ws.model.response.OrderDetailsResponse;
+import com.food.delivery.app.ws.service.OrderService;
+import com.food.delivery.app.ws.shared.dto.OrderDto;
 
 @RestController
 @RequestMapping("/orders")
 public class OrderController {
+	
+	@Autowired
+	OrderService orderService;
 
 	@GetMapping
 	public String getOrder() {
@@ -17,10 +29,20 @@ public class OrderController {
 	}
 	
 	@PostMapping
-	public String createOrder() {
-		return "create order method is called";
+	public OrderDetailsResponse createOrder(@RequestBody OrderDetailsRequestModel order) {
+		
+		OrderDetailsResponse returnValue = new OrderDetailsResponse();
+		ModelMapper modelMapper = new ModelMapper();
+		
+		OrderDto orderDto = new OrderDto();
+		BeanUtils.copyProperties(order, orderDto);
+		
+		OrderDto createdOrder = orderService.createOrder(orderDto);
+		returnValue = modelMapper.map(createdOrder, OrderDetailsResponse.class);
+		
+		return returnValue;
 	}
-	
+		
 	@PutMapping
 	public String updateOrder() {
 		return "update order method is called";
