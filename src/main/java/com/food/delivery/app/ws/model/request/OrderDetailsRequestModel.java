c4d85ed9754bb00a786e895ad6f115c4a4e3f40c
shehand <sonodewcomplex@gmail.com>
2019-06-25 21:12:50 +0530

    Add order request model

--- src/main/java/com/food/delivery/app/ws/model/request/OrderDetailsRequestModel.java
+++ src/main/java/com/food/delivery/app/ws/model/request/OrderDetailsRequestModel.java
@@ -4,6 +4,15 @@ public class OrderDetailsRequestModel {
 
 	private String[] items;
 	private float cost;
+	private String userId;
+
+	public float getCost() {
+		return cost;
+	}
+
+	public void setCost(float cost) {
+		this.cost = cost;
+	}
 
 	public String[] getItems() {
 		return items;
@@ -13,12 +22,12 @@ public class OrderDetailsRequestModel {
 		this.items = items;
 	}
 
-	public float getCost() {
-		return cost;
+	public String getUserId() {
+		return userId;
 	}
 
-	public void setCost(float cost) {
-		this.cost = cost;
+	public void setUserId(String userId) {
+		this.userId = userId;
 	}
 
 }
