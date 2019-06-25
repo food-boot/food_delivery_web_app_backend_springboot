980962b3df45d6b0c7e0d3b2708985d6d3acac8a
shehand <sonodewcomplex@gmail.com>
2019-06-25 21:14:13 +0530

    Add order dto clss

---
+++ src/main/java/com/food/delivery/app/ws/shared/dto/OrderDto.java
@@ -0,0 +1,54 @@
+package com.food.delivery.app.ws.shared.dto;
+
+import java.io.Serializable;
+
+public class OrderDto implements Serializable {
+
+	private static final long serialVersionUID = 2500955288592764755L;
+	private long id;
+	private String orderId;
+	private float cost;
+	private String[] items;
+	private String userId;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getOrderId() {
+		return orderId;
+	}
+
+	public void setOrderId(String orderId) {
+		this.orderId = orderId;
+	}
+
+	public float getCost() {
+		return cost;
+	}
+
+	public void setCost(float cost) {
+		this.cost = cost;
+	}
+
+	public String [] getItems() {
+		return items;
+	}
+
+	public void setItems(String [] items) {
+		this.items = items;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+}
