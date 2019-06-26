efcf0bf1ae11cc123e342ada6fe51601e6129184
shehand <sonodewcomplex@gmail.com>
2019-06-26 19:36:32 +0530

    Add get users method

--- src/main/java/com/food/delivery/app/ws/service/UserService.java
+++ src/main/java/com/food/delivery/app/ws/service/UserService.java
@@ -1,5 +1,7 @@
 package com.food.delivery.app.ws.service;
 
+import java.util.List;
+
 import org.springframework.security.core.userdetails.UserDetailsService;
 
 import com.food.delivery.app.ws.shared.dto.UserDto;
@@ -11,4 +13,5 @@ public interface UserService extends UserDetailsService{
 	UserDto getUserByUserId(String userId);
 	UserDto updateUser(String userId, UserDto user);
 	void deleteUser(String userId);
+	List<UserDto> getUsers();
 }
